485c7b2615664512cf8209456b33ebbd88adf6e2
Idan <idan.hovav16@gmail.com>
2016-04-03 17:34:18 -0700

    more progress, just have to get pic to show and to resize accurately

--- server.py
+++ server.py
@@ -1,30 +1,48 @@
 import os, sys
-from flask import Flask, render_template, g
-import urllib
-#import org.apache.commons.io.FilenameUtils;
-import Image
+from flask import Flask, render_template, flash, redirect, session
+from PIL import Image
+from flask.ext.wtf import Form
+from wtforms.fields import StringField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.secret_key = 'KJHDJKHKSANM<*&(*^21ngjk574754' # not sure why i need this
+imageFolderName = "images/"
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
-	return render_template('home.html')
+	form = ImageForm(csrf_enabled=False) # off because localhost
+	if form.validate_on_submit():
+		fileName = form.fileName.data
+		width = form.width.data
+		height = form.height.data
+		flash('Resizing Image={0} to {1}, {2}'.format(fileName, str(width), str(height)))
+		newImg = "imageResize/" + resize_function(fileName, width, height)
+		return resize(newImg)
+	return render_template('home.html', form=form)
 
 @app.route('/resize')
-def resize():
+def resize(img):
 	#resize the goddam image and return it to somewhere
-	IMAGE = [0]
-	return render_template('resize.html', image=IMAGE)
+	return render_template('resize.html', image=[img])
 
-def resize_function(inFile):
-	size = 128, 128
+# returns filename
+def resize_function(inFile, width, height):
+	size = int(float(width)), int(float(height))
 	inFileName, inExtension = os.path.splitext(inFile)
-	outFile = inFileName + ".thumbnail"
+	outFile = imageFolderName + inFileName + "_RESIZED"+ ".jpg"
 	try:
-		im = Image.open(inFile)
+		im = Image.open(imageFolderName + inFile)
 		im.thumbnail(size, Image.ANTIALIAS)
 		im.save(outFile, "JPEG")
+		return outFile
 	except IOError:
 		print("issue")
+
+class ImageForm(Form):
+	fileName = StringField('fileName', validators=[DataRequired()])
+	width = StringField('width', validators=[DataRequired()])
+	height = StringField('height', validators=[DataRequired()])
+
 if __name__ == '__main__':
 	app.run(debug=True)
